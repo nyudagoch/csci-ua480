86b870a08d52231892a1b9051b71457df35723d0
FrankJiang1208 <39501874+FrankJiang1208@users.noreply.github.com>
2018-06-25 18:01:41 -0400

    Assignment03_SJ1948 (#73)
    * Add files via upload
    
    * My message
    
    * Add files via upload
    
    * added folders for Assignment_04
    
    * Assignment_04 Player Movement Examples
    
    * modified player move-to-target example; fixed gitignore
    
    * Tilting Movement Example
    
    * reconnect scripts to objects in movement examples
    
    * Create Assignments_03
    
    * Delete Assignments_03
    
    * Add files via upload
    
    * changes
    
    * Add files via upload

---
+++ Assets/Assignments/Assignment_01/A01_sj1948/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A01
+{
+    public class Move : MonoBehaviour
+    {
+
+        public float speed = 1;
+        public float amount = 1;
+        public float offset = 0;
+        public float x;
+        public float z;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            transform.localPosition = new Vector3(x, Mathf.Sin(offset + Time.time * speed) * amount, z);
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_01/_A01_Master/Scripts/Rotate (copy).cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Vector3 beginning;
+    public Vector3 end;
+    float counter;
+    // properties
+    // Transform transform
+    // GameObject gameObject
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        counter += Time.deltaTime;
+        //Debug.Log(transform.eulerAngles);
+
+        // calculate rotation for each frame
+        transform.position = Vector3.Lerp(beginning, end, counter);
+	}
+}
